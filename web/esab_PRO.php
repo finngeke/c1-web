2aad71065048969d2287f8f63abd12f038efba6e
Roberto Pérez <lperezs@ripley.com>
2018-08-16 09:55:22 -0300

    Archivos Conexión a base de datos
    Se limpia el código, para dejar sólo la conexión que corresponde.

--- web/esab_PRO.php
+++ web/esab_PRO.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
-define('_SERVER_', '10.0.150.122');
-define('_USR_', 'CDV_ADM');
-define('_PWD_', 'CDV_ADM_DEPLOY');
-define('_DB_', 'saccqa');
-define('_TNSNAMES_', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL = TCP)(HOST = 10.0.150.122)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=saccqa)))');
-*/
-
-
 define('_SERVER_', 'dbccoradev.cnfepq2zma9q.sa-east-1.rds.amazonaws.com');
 define('_USR_', 'CDV_ADM');
 define('_PWD_', 'cdvadm');
--- web/esab_QA.php
+++ web/esab_QA.php
@@ -1,17 +1,7 @@
 <?php
 
-
 define('_SERVER_', '10.0.150.122');
 define('_USR_', 'CDV_ADM');
 define('_PWD_', 'CDV_ADM_DEPLOY');
 define('_DB_', 'saccqa');
 define('_TNSNAMES_', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL = TCP)(HOST = 10.0.150.122)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=saccqa)))');
-
-
-/*
-define('_SERVER_', 'dbccoradev.cnfepq2zma9q.sa-east-1.rds.amazonaws.com');
-define('_USR_', 'CDV_ADM');
-define('_PWD_', 'cdvadm');
-define('_DB_', 'DBC1DEV');
-define('_TNSNAMES_', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL = TCP)(HOST = dbccoradev.cnfepq2zma9q.sa-east-1.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=DBC1DEV)))');
-*/
