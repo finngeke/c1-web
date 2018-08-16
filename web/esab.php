ccbc22462dd55613236dcd17783f07ec7fb0cfff
Roberto Pérez <lperezs@ripley.com>
2018-08-16 10:26:24 -0300

    Archivo Base Conexiones
    Este archivo de remplaza por dos archivos separados de conexión. (QA y PRO)

--- web/esab.php
+++ web/esab.php
@@ -6,7 +6,7 @@ define('_USR_', 'CDV_ADM');
 define('_PWD_', 'CDV_ADM_DEPLOY');
 define('_DB_', 'saccqa');
 define('_TNSNAMES_', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL = TCP)(HOST = 10.0.150.122)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=saccqa)))');
-*/
+
 
 
 define('_SERVER_', 'dbccoradev.cnfepq2zma9q.sa-east-1.rds.amazonaws.com');
@@ -14,4 +14,4 @@ define('_USR_', 'CDV_ADM');
 define('_PWD_', 'cdvadm');
 define('_DB_', 'DBC1DEV');
 define('_TNSNAMES_', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL = TCP)(HOST = dbccoradev.cnfepq2zma9q.sa-east-1.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=DBC1DEV)))');
-
+*/
