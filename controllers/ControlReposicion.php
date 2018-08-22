fe822f5c496fc0d75bc9c7c2ee7f4f8c4bf23dba
José Miguel Candia Hernández <jmcandia@gmail.com>
2018-08-22 20:18:15 -0300

    Se modifica el mensaje de error en la bajada de embarque

--- controllers/ControlReposicion.php
+++ controllers/ControlReposicion.php
@@ -338,7 +338,7 @@
 					if (strtoupper($response->Body->fault->faultString) != "OK") {
 						$msj = $response->Body->fault->faultString;
 						header("Content-Type: application/json");
-						echo json_encode(array("estado" => -2, "mensaje" => "No se pudo aperturar la sesión: $msj"), JSON_PRETTY_PRINT);
+						echo json_encode(array("estado" => -2, "mensaje" => "No se pudo aperturar la sesión: $msj\n\nPor favor intente nuevamente. Si el error persiste, informe al administrador de sistema."), JSON_PRETTY_PRINT);
 						exit();
 					} else {
 						$id_sesion = $response->Body->sessionid;
