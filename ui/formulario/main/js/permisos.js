0c44b55829bf458494afe5a1a524563d6afb158b
Roberto Pérez <lperezs@ripley.com>
2018-09-21 12:15:45 -0300

    Mejoras Módulo Permisos
    Mejoas a despliege de información, despliega un loader al momento de listar y almacenar la informaciòn .  (Antes no se podia saber cuando estaba toda la infor listada o almacenada)

--- ui/formulario/main/js/permisos.js
+++ ui/formulario/main/js/permisos.js
@@ -1,4 +1,3 @@
-
 $(window).on('load', function () {
 
     $('#guardar_permisos_modulo_acciones').hide();
@@ -15,25 +14,25 @@ $(window).on('load', function () {
     $.getJSON(cargar_tipo_usuario, function (data) {
 
         /* se inicia el each que llenara los tipos de usuarios */
-        $.each(data, function (i,o) {
+        $.each(data, function (i, o) {
             $('#tabla_tipo_usuario').append(
-                    '<li><label class="tree-toggle nav-header"><a href="#" onclick="cargar_tabla_modulos(&quot;'+o[0]+'&quot;)">'+o[1]+'</a></label>'+
-                        '<ul id="ul_usu_'+o[0]+'" class="nav nav-list tree">'+
-                        '</ul>'+
+                '<li><label class="tree-toggle nav-header"><a href="#" onclick="cargar_tabla_modulos(&quot;' + o[0] + '&quot;)">' + o[1] + '</a></label>' +
+                '<ul id="ul_usu_' + o[0] + '" class="nav nav-list tree">' +
+                '</ul>' +
                 '</li>'
-                );
+            );
             inc_tipo_usu++;
         });
 
         /* FIN DEL each que llenara los tipos de usuarios */
 
         /*Each por cada tipo de usuaro llenara los usuarios que corresponden*/
-       $.each(data, function (i,o) {
-            $.getJSON(cargar_usuario,{ID_TIPO_USUARIO:o[0]}, function (data_usu) {
-                $.each(data_usu, function (a,b) {
+        $.each(data, function (i, o) {
+            $.getJSON(cargar_usuario, {ID_TIPO_USUARIO: o[0]}, function (data_usu) {
+                $.each(data_usu, function (a, b) {
                     var codigo_usuario_permiso = b[0];
-                    $("#ul_usu_"+o[0]).append(
-                        '<li value="' + b[0] + '"><a href="#" onclick="cargar_tabla_depto(&quot;'+codigo_usuario_permiso+'&quot;)">' + b[1] + '</a></li>'
+                    $("#ul_usu_" + o[0]).append(
+                        '<li value="' + b[0] + '"><a href="#" onclick="cargar_tabla_depto(&quot;' + codigo_usuario_permiso + '&quot;)">' + b[1] + '</a></li>'
                     );
                 });
             });
@@ -52,8 +51,6 @@ $(window).on('load', function () {
     /*############################# FIN DE agregar la etiqueta de ul fuera del ciclo ##################################*/
 
 
-
-
 }); //final de onload
 
 $('#guardar_permisos_usuarios').on('click', function () {
@@ -64,7 +61,11 @@ $('#guardar_permisos_usuarios').on('click', function () {
     if (respuesta_usuarios_permiso == true) {
 
         // Desabilito el BTN guardar
-        //$('#guardar_permisos_usuarios').attr('disable',true);
+        $('#guardar_permisos_modulo_acciones').fadeOut();
+        $('#guardar_permisos_usuarios').fadeOut();
+        $('.regresar').fadeOut();
+        // Desplegamos POPUP
+        $('#popup_guarda_permisos').modal('show');
 
         // Limpiamos la búsqueda para que el código recorra la tabla completa
         var table = $('#tabla_depto_permiso').DataTable();
@@ -73,22 +74,27 @@ $('#guardar_permisos_usuarios').on('click', function () {
         $("#tabla_depto_permiso >tbody >tr").each(function () {
 
             var depto_permiso_actualizado = $(this).find("td:eq(4)").text();
-            var cod_usu =  $("#cod_usu_hidden").html();
-            cod_usu = cod_usu.replace(/[^a-z0-9\,]/gi,'');
+            var cod_usu = $("#cod_usu_hidden").html();
+            cod_usu = cod_usu.replace(/[^a-z0-9\,]/gi, '');
 
             var dep_depto = $(this).find("td:eq(1)").text();
-            dep_depto = dep_depto.replace(/[^a-z0-9\,]/gi,'');
+            dep_depto = dep_depto.replace(/[^a-z0-9\,]/gi, '');
 
             var estado = $(this).find("td:eq(3) select").val();
-            var flag =1;
-                if (depto_permiso_actualizado == 'U'){
+            var flag = 1;
+            if (depto_permiso_actualizado == 'U') {
 
-                    var url_guarda_permiso = 'permiso_usuario/guardar_permiso_depto';
-                    $.getJSON(url_guarda_permiso,{COD_USU:cod_usu,DEP_DEPTO:dep_depto,ESTADO:estado,FLAG:flag},function (data) {
-                        $('#flag_data_insert_deptos').html(data);
-                    });
+                var url_guarda_permiso = 'permiso_usuario/guardar_permiso_depto';
+                $.getJSON(url_guarda_permiso, {
+                    COD_USU: cod_usu,
+                    DEP_DEPTO: dep_depto,
+                    ESTADO: estado,
+                    FLAG: flag
+                }, function (data) {
+                    $('#flag_data_insert_deptos').html(data);
+                });
 
-                }
+            }
         });
 
         // Nueva función, corrige el guardado... la función comentada recargaba la página antes de terminar si la cantidad de permisos a asignar eran muchos.
@@ -126,6 +132,14 @@ $('#guardar_permisos_modulo_acciones').on('click', function () {
 
     if (respuesta_modulo_acceso == true) {
 
+        // Desabilito el BTN guardar
+        $('#guardar_permisos_modulo_acciones').fadeOut();
+        $('#guardar_permisos_usuarios').fadeOut();
+        $('.regresar').fadeOut();
+        // Desplegamos POPUP
+        $('#popup_guarda_permisos').modal('show');
+
+
         $("#tabla_modulo_acceso >tbody >tr").each(function () {
 
             var modulo_permiso_actualizado = $(this).find("td:eq(2)").text();
@@ -136,24 +150,30 @@ $('#guardar_permisos_modulo_acciones').on('click', function () {
                 arregloDeSubCadenas = cadena.split(separador);
 
             var id_modulo_separado = arregloDeSubCadenas [2];
-            var id_accion_separado =  arregloDeSubCadenas [3];
+            var id_accion_separado = arregloDeSubCadenas [3];
 
             var estado_accion = $(this).find("td:eq(1) select").val();
 
-                if (id_accion_separado == null) {
-                    id_accion_separado = 0;
-                    estado_accion = 0;
-                }
+            if (id_accion_separado == null) {
+                id_accion_separado = 0;
+                estado_accion = 0;
+            }
 
-            var id_tipo_usu =  $("#flag_top_menu_tipo_usuario").text();
-            id_tipo_usu = id_tipo_usu.replace(/[^a-z0-9\,]/gi,'');
+            var id_tipo_usu = $("#flag_top_menu_tipo_usuario").text();
+            id_tipo_usu = id_tipo_usu.replace(/[^a-z0-9\,]/gi, '');
 
             var estado_modulo = $(this).find("td:eq(1) select").val();
 
-            if (modulo_permiso_actualizado == 'U'){
-               $.getJSON(url_guarda_permiso_modulo_accion,{ID_ACCION:id_accion_separado,TIP_USR:id_tipo_usu,ESTADO_ACCION:estado_accion,ID_MODULO:id_modulo_separado,ESTADO_MODULO:estado_modulo}, function (data) {
-                  $('#flag_data_insert_modulos').html(data);
-               });
+            if (modulo_permiso_actualizado == 'U') {
+                $.getJSON(url_guarda_permiso_modulo_accion, {
+                    ID_ACCION: id_accion_separado,
+                    TIP_USR: id_tipo_usu,
+                    ESTADO_ACCION: estado_accion,
+                    ID_MODULO: id_modulo_separado,
+                    ESTADO_MODULO: estado_modulo
+                }, function (data) {
+                    $('#flag_data_insert_modulos').html(data);
+                });
             }
 
         });
@@ -178,28 +198,31 @@ $('#guardar_permisos_modulo_acciones').on('click', function () {
         }, delay_validar_guardado);*/
 
 
-    }else{
+    } else {
         alert("No se han realizado modificaciones.");
     }
 
 });
 
-function campo_ajustado(inc){
+function campo_ajustado(inc) {
 
-    $("#col_act_estado_depto_"+inc).html('U');
+    $("#col_act_estado_depto_" + inc).html('U');
 }
 
-function campo_flag_modulo(inc_modulo){
+function campo_flag_modulo(inc_modulo) {
 
-    $("#flag_modulo_"+inc_modulo).html('U');
+    $("#flag_modulo_" + inc_modulo).html('U');
 }
 
-function campo_flag_acciones(inc_modulo,inc_accion){
+function campo_flag_acciones(inc_modulo, inc_accion) {
     //$('#select_modulo_accion_'+b[0]+'').attr("enable","enable");
-    $("#flag_accion_"+inc_modulo+"_"+inc_accion).html('U');
+    $("#flag_accion_" + inc_modulo + "_" + inc_accion).html('U');
 }
 
 function cargar_tabla_depto(codigo_usu_permiso) {
+
+    // Despliega POPUP de carga de contenido/permisos
+    $('#popup_carga_permisos').modal('show');
 
     $('#guardar_permisos_usuarios').show();
     $('#guardar_permisos_modulo_acciones').hide();
@@ -208,40 +231,40 @@ function cargar_tabla_depto(codigo_usu_permiso) {
     $('#tabla_depto_permiso tbody').empty();
 
     var div1 = document.getElementById("div_tabla_depto_permiso");
-        div1.style.display = "";
+    div1.style.display = "";
 
     var div2 = document.getElementById("div_tabla_modulo_acceso");
-        div2.style.display = "none";
+    div2.style.display = "none";
 
     $('#cod_usu_hidden').html(codigo_usu_permiso);
     var cargar_tabla_depto_permisos = 'permiso_usuario/llenar_tabla_depto_permisos';
-    var inc_depto_permisos = 0 ;
+    var inc_depto_permisos = 0;
 
     // se construye la segunda tabla cada vez que seleccione un usuario//
-    $.getJSON(cargar_tabla_depto_permisos,{CODIGO_USUARIO_PERMISO:codigo_usu_permiso},function (data_depto) {
-        $.each(data_depto,function (r,d) {
+    $.getJSON(cargar_tabla_depto_permisos, {CODIGO_USUARIO_PERMISO: codigo_usu_permiso}, function (data_depto) {
+        $.each(data_depto, function (r, d) {
             $('#tabla_depto_permiso').append(
                 '<tr>' +
-                '<td>'+d[0]+'</td>'+
-                '<td>'+d[1]+'</td>'+
-                '<td>'+d[2]+'</td>'+
-                '<td><select id="selec_estado_depto_'+inc_depto_permisos+'" name="selec_estado_depto"  onchange="campo_ajustado('+inc_depto_permisos+');">' +
+                '<td>' + d[0] + '</td>' +
+                '<td>' + d[1] + '</td>' +
+                '<td>' + d[2] + '</td>' +
+                '<td><select id="selec_estado_depto_' + inc_depto_permisos + '" name="selec_estado_depto"  onchange="campo_ajustado(' + inc_depto_permisos + ');">' +
                 '<option value="0" >No Permitido</option>' +
                 '<!-- <option value="1" >Lectura</option> -->' +
                 '<option value="2" >Permitido</option>' +
                 '</select></td>' +
-                '<td id="col_act_estado_depto_'+inc_depto_permisos+'" style="display: none;"></td>' +
+                '<td id="col_act_estado_depto_' + inc_depto_permisos + '" style="display: none;"></td>' +
                 '</tr>');
-            $("#selec_estado_depto_"+inc_depto_permisos).val(d[3]);
+            $("#selec_estado_depto_" + inc_depto_permisos).val(d[3]);
             inc_depto_permisos++;
         });
 
         /* La tabla toma las propiedades de datatable con el fin de mantener los titulos fijos*/
-        var delay_thead = 1000;
+        var delay_thead = 2000;
         setTimeout(function () {
             $('#tabla_depto_permiso').DataTable({
                 retrieve: true,
-                destroy :true,
+                destroy: true,
                 paging: false,
                 scrollY: "400px",
                 scrollX: "500px",
@@ -249,21 +272,31 @@ function cargar_tabla_depto(codigo_usu_permiso) {
                 scrollCollapse: true,
                 "oLanguage": {
                     "sSearch": "Buscar:",
-                    "sZeroRecords" : "No se encontraron registros"
+                    "sZeroRecords": "No se encontraron registros"
                 }
             });
 
-            $('#tabla_depto_permiso_filter label input').attr ('id', 'filtro_perfil_usuario');
+            $('#tabla_depto_permiso_filter label input').attr('id', 'filtro_perfil_usuario');
 
-        },delay_thead);
+        }, delay_thead);
 
     });
+
+    // Define Tiempo 1 = 1000
+    var delay = 5000;
+    setTimeout(function () {
+        // Ocultar POPUP de carga de contenido/permisos
+        $('#popup_carga_permisos').modal('hide');
+    }, delay);
 
 }
 
 function cargar_tabla_modulos(tip_usr) {
 
-    if(tip_usr != 99) {
+    if (tip_usr != 99) {
+
+        // Despliega POPUP de carga de contenido/permisos
+        $('#popup_carga_permisos').modal('show');
 
         $('#flag_top_menu_tipo_usuario').html(tip_usr);
 
@@ -274,10 +307,10 @@ function cargar_tabla_modulos(tip_usr) {
         $('#tabla_depto_permiso tbody').empty();
 
         var div1 = document.getElementById("div_tabla_depto_permiso");
-            div1.style.display = "none";
+        div1.style.display = "none";
 
         var div2 = document.getElementById("div_tabla_modulo_acceso");
-            div2.style.display = "";
+        div2.style.display = "";
 
         var url_cargar_modulos = 'permiso_usuario/cargar_modulos';
         var url_cargar_modulos_acciones = 'permiso_usuario/cargar_modulos_acciones';
@@ -321,7 +354,7 @@ function cargar_tabla_modulos(tip_usr) {
                         $('#tr_modulo_' + o[0]).after(
                             '<tr>' +
 
-                                '<td>' + b[2] + '</td>' +
+                            '<td>' + b[2] + '</td>' +
 
                             '<td>' +
                             '<select id="select_modulo_accion_' + b[0] + '" onchange="campo_flag_acciones(' + o[0] + ',' + b[0] + ')">' +
@@ -376,7 +409,15 @@ function cargar_tabla_modulos(tip_usr) {
 
         });
 
-    }else{
+        // Define Tiempo 1 = 1000
+        var delay = 10000;
+        setTimeout(function () {
+            // Ocultar POPUP de carga de contenido/permisos
+            $('#popup_carga_permisos').modal('hide');
+        }, delay);
+
+
+    } else {
 
         $('#tabla_modulo_acceso tbody').empty();
         $('#tabla_depto_permiso tbody').empty();
