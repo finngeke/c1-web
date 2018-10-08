b1b923eee00645400941114a6df955c68edbd4bb
José Miguel Candia Hernández <jmcandia@gmail.com>
2018-10-08 14:48:24 -0300

    Se comenta una validación en la carga de los packing list

--- controllers/ControlProveedor.php
+++ controllers/ControlProveedor.php
@@ -657,7 +657,7 @@
 					}
 					
 					// Valida los costos por PO
-					$etapa = "validatePOAmount";
+					/*$etapa = "validatePOAmount";
 					foreach ($temp_m as $llave => $valor) {
 						//TODO: Busca los montos de la PO
 						$r = \proveedor\proveedor::getInfoPO($llave);
@@ -688,7 +688,7 @@
 							$f3->set('SESSION.error', "The quantity of units in the packing list can not be greater than total units loaded to purchase order number '$llave'. Please check de file.");
 							$f3->reroute("/invoices?cod_proveedor=$cod_proveedor");
 						}
-					}
+					}*/
 					
 					$etapa = "clearInvoicePLData";
 					foreach ($registros as $registro) {
