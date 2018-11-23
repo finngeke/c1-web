6c3a890cbb86668c558a58c0f2e09d534d42ceca
Robeto Pérez <25640100+lrobertops@users.noreply.github.com>
2018-11-23 09:40:23 -0300

    Add files via upload

--- controllers/simulador_compra/PlanCompraController.php
+++ controllers/simulador_compra/PlanCompraController.php
@@ -19,11 +19,16 @@ class PlanCompraController extends \Control
 
 
     // Actualiza Plan de Compra
-    public function ActualizaPlanCompra($f3)
+    public function ProcesaDataPlanCompra($f3)
     {
 
         $tempData = html_entity_decode($_REQUEST['models']);
         $json = json_decode($tempData, true);
+
+
+        // var_dump($json['updated']);
+        // die();
+
 
         // Recorrer el JSON
         foreach ($json['updated'] as $columna) {
@@ -51,7 +56,7 @@ class PlanCompraController extends \Control
             $CALIDAD = $columna["CALIDAD"];
             $COD_OCASION_USO = $columna["COD_OCASION_USO"];
             $COD_PIRAMIX = $columna["COD_PIRAMIX"];
-            $DESCRIPCION = $columna["DESCRIPCION"];
+            $NOM_VENTANA = $columna["NOM_VENTANA"];
             $COD_RANKVTA = $columna["COD_RANKVTA"];
             $LIFE_CYCLE = $columna["LIFE_CYCLE"];
             $NUM_EMB = $columna["NUM_EMB"];
@@ -132,11 +137,20 @@ class PlanCompraController extends \Control
             $COSTO_FOB_BASE = $columna["COSTO_FOB_BASE"];
             $COSTO_INSP_BASE = $columna["COSTO_INSP_BASE"];
             $COSTO_RFID_BASE = $columna["COSTO_RFID_BASE"];
+            $COD_MARCA = $columna["COD_MARCA"];
+            $N_CURVASXCAJAS = $columna["N_CURVASXCAJAS"];
+            $COD_JER2 = $columna["COD_JER2"];
+            $COD_SUBLIN = $columna["COD_SUBLIN"];
 
 
 
 
-            echo json_encode(\simulador_compra\PlanCompraClass::ActualizaPlanCompra($f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO'), $f3->get('SESSION.login'),$ID_COLOR3,$GRUPO_COMPRA,$COD_TEMP,$LINEA,$SUBLINEA,$MARCA,$ESTILO,$SHORT_NAME,$ID_CORPORATIVO,$DESCMODELO,$DESCRIP_INTERNET,$NOMBRE_COMPRADOR,$NOMBRE_DISENADOR,$COMPOSICION,$TIPO_TELA,$FORRO,$COLECCION,$EVENTO,$COD_ESTILO_VIDA,$CALIDAD,$COD_OCASION_USO,$COD_PIRAMIX,$DESCRIPCION,$COD_RANKVTA,$LIFE_CYCLE,$NUM_EMB,$COD_COLOR,$TIPO_PRODUCTO,$TIPO_EXHIBICION,$DESTALLA,$TIPO_EMPAQUE,$PORTALLA_1_INI,$PORTALLA_1,$CURVATALLA,$CURVAMIN,$UNID_OPCION_INICIO,$UNID_OPCION_AJUSTADA,$CAN,$MTR_PACK,$CANT_INNER,$SEG_ASIG,$FORMATO,$TDAS,$A,$B,$C,$I,$UND_ASIG_INI,$ROT,$NOM_PRECEDENCIA,$NOM_VIA,$NOM_PAIS,$VIAJE,$MKUP,$PRECIO_BLANCO,$OFERTA,$GM,$COD_TIP_MON,$COSTO_TARGET,$COSTO_FOB,$COSTO_INSP,$COSTO_RFID,$ROYALTY_POR,$COSTO_UNIT,$COSTO_UNITS,$CST_TOTLTARGET,$COSTO_TOT,$COSTO_TOTS,$RETAIL,$DEBUT_REODER,$SEM_INI,$SEM_FIN,$CICLO,$AGOT_OBJ,$SEMLIQ,$ALIAS_PROV,$COD_PROVEEDOR,$COD_TRADER,$AFTER_MEETING_REMARKS,$CODSKUPROVEEDOR,$SKU,$PROFORMA,$ARCHIVO,$ESTILO_PMM,$ESTADO_MATCH,$PO_NUMBER,$ESTADO_OC,$FECHA_ACORDADA,$FECHA_EMBARQUE,$FECHA_ETA,$FECHA_RECEPCION,$DIAS_ATRASO,$CODESTADO,$ESTADO_C1,$VENTANA_LLEGADA,$PROFORMA_BASE,$TIPO_EMPAQUE_BASE,$UNI_INICIALES_BASE,$PRECIO_BLANCO_BASE,$COSTO_TARGET_BASE,$COSTO_FOB_BASE,$COSTO_INSP_BASE,$COSTO_RFID_BASE));
+            echo json_encode(\simulador_compra\PlanCompraClass::ActualizaPlanCompra(
+                $f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO'), $f3->get('SESSION.login'),
+                $ID_COLOR3,$GRUPO_COMPRA,$COD_TEMP,$LINEA,$SUBLINEA,$MARCA,$ESTILO,$SHORT_NAME,$ID_CORPORATIVO,$DESCMODELO,$DESCRIP_INTERNET,
+                $NOMBRE_COMPRADOR,$NOMBRE_DISENADOR,$COMPOSICION,$TIPO_TELA,$FORRO,$COLECCION,$EVENTO,$COD_ESTILO_VIDA,$CALIDAD,$COD_OCASION_USO,
+                $COD_PIRAMIX,$NOM_VENTANA,$COD_RANKVTA,$LIFE_CYCLE,$NUM_EMB,$COD_COLOR,$TIPO_PRODUCTO,$TIPO_EXHIBICION,$DESTALLA,$TIPO_EMPAQUE,
+                $PORTALLA_1_INI,$PORTALLA_1,$CURVATALLA,$CURVAMIN,$UNID_OPCION_INICIO,$UNID_OPCION_AJUSTADA,$CAN,$MTR_PACK,$CANT_INNER,$SEG_ASIG,$FORMATO,$TDAS,$A,$B,$C,$I,$UND_ASIG_INI,$ROT,$NOM_PRECEDENCIA,$NOM_VIA,$NOM_PAIS,$VIAJE,$MKUP,$PRECIO_BLANCO,$OFERTA,$GM,$COD_TIP_MON,$COSTO_TARGET,$COSTO_FOB,$COSTO_INSP,$COSTO_RFID,$ROYALTY_POR,$COSTO_UNIT,$COSTO_UNITS,$CST_TOTLTARGET,$COSTO_TOT,$COSTO_TOTS,$RETAIL,$DEBUT_REODER,$SEM_INI,$SEM_FIN,$CICLO,$AGOT_OBJ,$SEMLIQ,$ALIAS_PROV,$COD_PROVEEDOR,$COD_TRADER,$AFTER_MEETING_REMARKS,$CODSKUPROVEEDOR,$SKU,$PROFORMA,$ARCHIVO,$ESTILO_PMM,$ESTADO_MATCH,$PO_NUMBER,$ESTADO_OC,$FECHA_ACORDADA,$FECHA_EMBARQUE,$FECHA_ETA,$FECHA_RECEPCION,$DIAS_ATRASO,$CODESTADO,$ESTADO_C1,$VENTANA_LLEGADA,$PROFORMA_BASE,$TIPO_EMPAQUE_BASE,$UNI_INICIALES_BASE,$PRECIO_BLANCO_BASE,$COSTO_TARGET_BASE,$COSTO_FOB_BASE,$COSTO_INSP_BASE,$COSTO_RFID_BASE,$COD_MARCA,$N_CURVASXCAJAS,$COD_JER2,$COD_SUBLIN));
 
 
 
@@ -146,7 +160,7 @@ class PlanCompraController extends \Control
         }
 
 
-        //var_dump($json['updated']);
+
 
 
         // Fin del actualizar grilla
@@ -190,12 +204,81 @@ class PlanCompraController extends \Control
         echo json_encode($varibles);
     }
 
-
     // Carga POPUP de Historial en Plan de Compra
     public function ListarHistorial($f3)
     {
         echo json_encode(\simulador_compra\PlanCompraClass::ListarHistorial($f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO'), $f3->get('GET.ID_COLOR3')));
     }
+
+    // Busca Factor
+    public function BuscaFactor($f3) { // $pais,$via,$moneda,$ventana
+        echo json_encode(\simulador_compra\PlanCompraClass::BuscaFactor($f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO'), $f3->get('GET.PAIS'), $f3->get('GET.VIA'), $f3->get('GET.MONEDA'), $f3->get('GET.VENTANA')));
+    }
+
+    // Busca Factor
+    public function BuscaTipoCambio($f3) { // ,$moneda,$ventana
+        echo json_encode(\simulador_compra\PlanCompraClass::BuscaTipoCambio($f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO'), $f3->get('GET.MONEDA'), $f3->get('GET.VENTANA')));
+    }
+
+
+    // Actualizar grilla en plan_compra_color3
+    public function actualiza_grilla_plan_compra_color3($f3) {
+
+        $COSTO_UNITS = round($f3->get('GET.COSTO_UNITS'),0);
+
+        /* ECHO ($f3->get('GET.und_ajust')."|".
+            $f3->get('GET.porcent_ajust')."|".
+            $f3->get('GET.n_cajas')."|".
+            $f3->get('GET.primera_carga')."|".
+            $f3->get('GET.tiendas')."|".
+            $f3->get('GET.unida_ajust_xtallas')."|".
+            $f3->get('GET.UNIDADES_FINALES')."|".
+            $f3->get('GET.marca_')."|".
+            $f3->get('GET.cluster_')."|".
+            $f3->get('GET.debut_')."|".
+            $f3->get('GET.tipo_emp_')."|".
+            $f3->get('GET.formatos_')."|".
+            $f3->get('GET.UNIDADES_INICIALES'));*/
+
+        $cluster = str_replace(" ","+",$f3->get('GET.cluster_'));
+
+        echo \simulador_compra\cbx_grilla_compra::actualiza_grilla_plan_compra_color3($f3->get('SESSION.COD_TEMPORADA')
+            , $f3->get('SESSION.COD_DEPTO'), $f3->get('SESSION.login'), $f3->get('GET.ID_COLOR3')
+            , $f3->get('GET.COSTO_FOB'), $f3->get('GET.COSTO_INSP'), $f3->get('GET.COSTO_RFID')
+            , $f3->get('GET.COSTO_UNIT'), $COSTO_UNITS, $f3->get('GET.CST_TOTLTARGET')
+            , $f3->get('GET.COSTO_TOT'), $f3->get('GET.COSTO_TOTS'), $f3->get('GET.MKUP')
+            , $f3->get('GET.GM'), $f3->get('GET.PROVEEDOR'), $f3->get('GET.VIA')
+            , $f3->get('GET.PAIS'), $f3->get('GET.FACTOR_EST'), $f3->get('GET.NOM_VIA')
+            , $f3->get('GET.NOM_PAIS'), $f3->get('GET.TARGET')
+            ,  $f3->get('GET.tipo_emp_'),$f3->get('GET.UNIDADES_INICIALES'),$f3->get('GET.und_ajust'),$f3->get('GET.UNIDADES_FINALES'),
+            $f3->get('GET.porcent_ajust'),$f3->get('GET.tiendas'),$f3->get('GET.formatos_'),$f3->get('GET.n_cajas'),
+            $f3->get('GET.unida_ajust_xtallas'),$f3->get('GET.marca_'),$cluster,$f3->get('GET.debut_'),$f3->get('GET.precioRetail_'),$f3->get('GET.precio_blanco_')
+            ,$f3->get('GET.COSTO'));
+    }
+
+    // Actualizar grilla en PLC_PLAN_COMPRA_COLOR_CIC
+    public function actualiza_grilla_plan_compra_color_cic($f3) {
+        echo \simulador_compra\cbx_grilla_compra::actualiza_grilla_plan_compra_color_cic($f3->get('SESSION.COD_TEMPORADA')
+            , $f3->get('SESSION.COD_DEPTO'), $f3->get('SESSION.login')
+            , $f3->get('GET.ID_COLOR3'), $f3->get('GET.COSTO')
+            , $f3->get('GET.precioRetail_'));
+    }
+
+    // Listar País
+    public function ListarPais($f3) {
+        echo json_encode(\simulador_compra\PlanCompraClass::ListarPais($f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO')));
+    }
+
+    // Listar Formatos Grilla Editar
+    public function ListarFormato($f3) {
+        echo json_encode(\simulador_compra\PlanCompraClass::ListarFormato($f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO')));
+    }
+
+    // Listar Ventana Grilla Editar
+    public function ListarVentana($f3) {
+        echo json_encode(\simulador_compra\PlanCompraClass::ListarVentana($f3->get('SESSION.COD_TEMPORADA'), $f3->get('SESSION.COD_DEPTO')));
+    }
+
 
 
 // Termina Clase
