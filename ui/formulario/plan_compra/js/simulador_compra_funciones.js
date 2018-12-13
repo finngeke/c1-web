f39acba09eb61c89a8f6187743b81165fdc2ae12
Robeto Pérez <25640100+lrobertops@users.noreply.github.com>
2018-12-13 14:55:08 -0300

    Ordena Llamada de JS

--- ui/formulario/plan_compra/js/simulador_compra_funciones.js
+++ ui/formulario/plan_compra/js/simulador_compra_funciones.js
@@ -1,37 +1,13 @@
 $(function () {
 
     // #############################################################################################
-    // ################################## AQUÍ VAN ACCIONES JS #####################################
-    // ################## AQUÍ SE DEFINE SOLO LA ESTRUCTURA DE POPUPS/BOTONES ######################
-    // ################################### SÓLO CODIGO TELERIK #####################################
-
-    // Actualiza Fecha de Concurrencia
-    function act_fecha_concurrencia() {
-
-        var url_act_fecha_concurrencia = 'TelerikPlanCompra/actualiza_fecha_concurrencia';
-
-        $.ajax({
-            type: "GET",
-            url: url_act_fecha_concurrencia,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function () {
-                // Acción al Ejecutarse correctamente
-                console.log("Se Actualiza Fecha de Concurrencia");
-            }, error: function (jqXHR, textStatus, errorThrown) {
-                console.log("Error: " + textStatus + " errorThrown: " + errorThrown);
-            }
-        }).done(function () {
-            console.log("Se realiza solicitud de Actualización de Fecha en Concurrencia");
-        });
-
-    }
+    // ################################## AQUÍ VAN FUNCIONES JS ####################################
+    // ############### MANTENER FORMATO DE NOMBRES EN JS - CONTROLADOR - CLASE  ####################
+    // ################################### Formato: CamelCase ######################################
 
     // Función Limpia Caracteres
-    function limpia_caracteres(string) {
-
+    function LimpiaCaracteres(string) {
         return string.replace(/[^a-z0-9\-\_]/gi, '-');
-
     }
 
 
