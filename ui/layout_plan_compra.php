555c2f4770094df32cd4e240e53058b1742b21c3
Robeto Pérez <lrobertops@users.noreply.github.com>
2018-12-26 17:54:02 -0300

    Versionamiento Dinámico

--- ui/layout_plan_compra.php
+++ ui/layout_plan_compra.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <title>[{{ @BD_CONEXION }}] - {{@TITLE}}</title>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <meta charset="utf-8">
     <link href="{{@TELERIK}}web/telerik/content/shared/styles/plan_compra.css" rel="stylesheet">
     <link href="{{@TELERIK}}web/telerik/styles/kendo.common.min.css" rel="stylesheet">
@@ -20,17 +23,17 @@
 <include href="{{ @contenido }}"/>
 
 <!-- 0 En Cargar (Orden Obligatorio) -->
-<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_permisos.js?v=A30"></script>
+<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_permisos.js?v={{rand(0,999)}}"></script>
 <!-- 1 En Cargar (Orden Obligatorio) -->
-<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_lectura.js?v=A30"></script>
+<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_lectura.js?v={{rand(0,999)}}"></script>
 <!-- 2 En Cargar (Orden Obligatorio) -->
-<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra.js?v=A30"></script>
+<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra.js?v={{rand(0,999)}}"></script>
 <!-- 3 En Cargar (Orden Obligatorio) -->
-<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_menutop.js?v=A30"></script>
+<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_menutop.js?v={{rand(0,999)}}"></script>
 <!-- 4 En Cargar (Orden Obligatorio) -->
-<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_celdabtn.js?v=A30"></script>
+<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_celdabtn.js?v={{rand(0,999)}}"></script>
 <!-- 5 En Cargar (Orden Obligatorio) -->
-<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_funciones.js?v=A30"></script>
+<script src="{{@TELERIK}}ui/formulario/plan_compra/js/simulador_compra_funciones.js?v={{rand(0,999)}}"></script>
 
 </body>
 </html>
