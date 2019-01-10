b0f52668fa53d88a1acf84dcd3442e74cfd449d5
Robeto Pérez <lrobertops@users.noreply.github.com>
2019-01-10 17:17:07 -0300

    Quita validación módulo

--- ui/formulario/plan_compra/js/simulador_compra_funciones.js
+++ ui/formulario/plan_compra/js/simulador_compra_funciones.js
@@ -6,44 +6,15 @@ $(function () {
     // ################################### Formato: CamelCase ######################################
 
     if(localStorage.getItem("M-TIENDA")==0){
-
-        // Verificar Permisos
-        if(localStorage.getItem("T0016")){
-
-            $("#poptienda_tipotienda").hide();
-            $("#poptienda_asignacion").hide();
-            $("#poptienda_btns").hide();
-            $("#btn_replica_temporada_tienda").hide();
-
             // Levantamos el popup
             var POPUPTiendaValidado = $("#POPUP_tienda");
             POPUPTiendaValidado.data("kendoWindow").open();
-
-        }else{
-            var popupNotification = $("#popupNotification").kendoNotification().data("kendoNotification");
-            popupNotification.getNotifications().parent().remove();
-            popupNotification.show(" Necesitas Configurar Tiendas... Pero Necesitas Permisos.", "error");
-            // Fin Verificar Permisos
-        }
-
     }
 
 
     if( (localStorage.getItem("P-COSTO")==0) || (localStorage.getItem("P-RETAIL")==0) || (localStorage.getItem("P-EMBARQUE")!=9) ){
-
-        // Verificar Permisos
-        if(localStorage.getItem("T0016")){
-
             var popupPreTotalValidado = $("#POPUP_presupuestos_total");
             popupPreTotalValidado.data("kendoWindow").open();
-
-        }else{
-            var popupNotification = $("#popupNotification").kendoNotification().data("kendoNotification");
-            popupNotification.getNotifications().parent().remove();
-            popupNotification.show(" Necesitas Configurar Presupuestos... Pero Necesitas Permisos.", "error");
-            // Fin Verificar Permisos
-        }
-
     }
 
 
