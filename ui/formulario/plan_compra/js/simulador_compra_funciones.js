53411ec2a203b073d3fb21a3a1711e10d3032f36
Robeto Pérez <lrobertops@users.noreply.github.com>
2019-01-10 17:08:50 -0300

    Valida Tienda y Presupuestos

--- ui/formulario/plan_compra/js/simulador_compra_funciones.js
+++ ui/formulario/plan_compra/js/simulador_compra_funciones.js
@@ -5,9 +5,45 @@ $(function () {
     // ############### MANTENER FORMATO DE NOMBRES EN JS - CONTROLADOR - CLASE  ####################
     // ################################### Formato: CamelCase ######################################
 
-    // Función Limpia Caracteres
-    function LimpiaCaracteres(string) {
-        return string.replace(/[^a-z0-9\-\_]/gi, '-');
+    if(localStorage.getItem("M-TIENDA")==0){
+
+        // Verificar Permisos
+        if(localStorage.getItem("T0016")){
+
+            $("#poptienda_tipotienda").hide();
+            $("#poptienda_asignacion").hide();
+            $("#poptienda_btns").hide();
+            $("#btn_replica_temporada_tienda").hide();
+
+            // Levantamos el popup
+            var POPUPTiendaValidado = $("#POPUP_tienda");
+            POPUPTiendaValidado.data("kendoWindow").open();
+
+        }else{
+            var popupNotification = $("#popupNotification").kendoNotification().data("kendoNotification");
+            popupNotification.getNotifications().parent().remove();
+            popupNotification.show(" Necesitas Configurar Tiendas... Pero Necesitas Permisos.", "error");
+            // Fin Verificar Permisos
+        }
+
+    }
+
+
+    if( (localStorage.getItem("P-COSTO")==0) || (localStorage.getItem("P-RETAIL")==0) || (localStorage.getItem("P-EMBARQUE")!=9) ){
+
+        // Verificar Permisos
+        if(localStorage.getItem("T0016")){
+
+            var popupPreTotalValidado = $("#POPUP_presupuestos_total");
+            popupPreTotalValidado.data("kendoWindow").open();
+
+        }else{
+            var popupNotification = $("#popupNotification").kendoNotification().data("kendoNotification");
+            popupNotification.getNotifications().parent().remove();
+            popupNotification.show(" Necesitas Configurar Presupuestos... Pero Necesitas Permisos.", "error");
+            // Fin Verificar Permisos
+        }
+
     }
 
 
