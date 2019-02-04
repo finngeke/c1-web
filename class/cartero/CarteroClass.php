b36e8da7b51e8e3718e608383b36b81e60e6b696
Robeto Pérez <lrobertops@users.noreply.github.com>
2019-02-04 14:16:53 -0300

    Corrige Clase Cartero

--- class/cartero/CarteroClass.php
+++ class/cartero/CarteroClass.php
@@ -18,7 +18,7 @@ class CarteroClass extends \parametros
         $mail->SMTPAuth = true;
         $mail->Host = "mail.ripley.com"; // A RELLENAR. Aquí pondremos el SMTP a utilizar. Por ej. mail.midominio.com
         $mail->Username = "lrobertop@ripley.com"; // A RELLENAR. Email de la cuenta de correo. ej.info@midominio.com La cuenta de correo debe ser creada previamente.
-        $mail->Password = "Ripley0119"; // A RELLENAR. Aqui pondremos la contraseña de la cuenta de correo
+        $mail->Password = "Ripley2019"; // A RELLENAR. Aqui pondremos la contraseña de la cuenta de correo
         $mail->Port = 465; // Puerto de conexión al servidor de envio.
         $mail->From = "lrobertop@ripley.com"; // A RELLENARDesde donde enviamos (Para mostrar). Puede ser el mismo que el email creado previamente.
         $mail->FromName = "Roberto Pérez"; //A RELLENAR Nombre a mostrar del remitente.
